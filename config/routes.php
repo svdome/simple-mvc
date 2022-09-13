92c30d506f0677a20824931cacb636a5befcee06
Evgeny Baryshnikov <svdome@yandex.ru>
2022-09-13 12:27:54 +0300

    .

--- config/routes.php
+++ config/routes.php
@@ -7,4 +7,4 @@ Router::add('^user/getall/?$', ['controller' => 'user', 'action' => 'getall']);
 Router::add('^user/getone/?$', ['controller' => 'user', 'action' => 'getone']);
 Router::add('^admin/?$', ['controller' => 'main', 'action' => 'index', 'admin_prefix' => 'admin']);
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
-//Router::add('^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?');
+Router::add('^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?');
